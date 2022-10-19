4bcf764ef8d4befcbe836573971d480497070044
Mariana R <anairamsor@hotmail.com>
2022-10-18 20:24:28 -0500

    Se simplificó la estructura del index.html y se reenombraron clases. Se corrigieron las clases en style.css y se trabajó el estilo utilizando flex. Por último, se modificó script.js para evitar el uso de mayúsculas y acentos. Anteriormente solo se modificaban.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var inputText = document.querySelector(".inputText")
-var displayText = document.querySelector(".displayText")
+var inputText = document.getElementById("inputText");
+var displayText = document.getElementById("displayText");
 let rules = [["e", "enter"],["i", "imes"],["a", "ai"],["o","ober"],["u", "utat"]]
 
+inputText.addEventListener("keypress", function (evt) {
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    if (charCode > 31 && (charCode < 97 || charCode > 122) && charCode != 241 && charCode != 32) {
+        evt.preventDefault();
+    }
+});
+
+
 function encriptar(encrString){
-    encrString = encrString.normalize("NFD").replace(/[^\w\s]/gi, '')
-    encrString = encrString.toLowerCase();
     for (var i = 0; i < rules.length; i++){
         if (encrString.includes(rules[i][0])){
                 encrString=encrString.replaceAll(rules[i][0],rules[i][1])
@@ -14,8 +20,6 @@ function encriptar(encrString){
 }
 
 function desencriptar(DencrString){
-    DencrString = DencrString.normalize("NFD").replace(/[^\w\s]/gi, '') 
-    DencrString = DencrString.toLowerCase();
     for (var i = 0; i < rules.length; i++){
         if (DencrString.includes(rules[i][1])){
                 DencrString=DencrString.replaceAll(rules[i][1],rules[i][0])
